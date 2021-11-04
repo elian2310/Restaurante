9052f7df80b801d83c3d664c6eab261e7f6bee89
nicopanozo <nicopanozo@DESKTOP-QQRQ7A5>
2021-11-03 21:51:40 -0400

    cambios en ventana

--- RestauranteIS/RestauranteIS/Form1.Designer.cs
+++ RestauranteIS/RestauranteIS/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace RestauranteIS
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(590, 229);
+            this.label1.Location = new System.Drawing.Point(360, 314);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 32);
             this.label1.TabIndex = 0;
